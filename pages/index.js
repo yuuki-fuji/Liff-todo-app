d3a9ac005f1ecba2629c204b1dfd964802449727
fujimoto <yuukikun666@gmail.com>
2023-05-26 00:37:04 +0900

    scriptの読み込みを修正

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home({ liff, liffError }) {
         <title>LIFF App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
+        <script async src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
       </Head>
 
       <main className={styles.main}>
@@ -28,7 +29,6 @@ export default function Home({ liff, liffError }) {
         >
           LIFF Documentation
         </a>
-        <script src="https://static.line-scdn.net/liff/edge/2.1/sdk.js"></script>
       </main>
     </div>
   );
